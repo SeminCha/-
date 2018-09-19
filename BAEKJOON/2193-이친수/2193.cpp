58a50cfa00e693d233ec423633f5a3cefa115fa6
SeminCha <cing9393@gmail.com>
2018-09-19 17:32:16 +0900

    [2193-이친수] dp로 해결, bottom up 방식

---
+++ BAEKJOON/2193-이친수/2193.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+long long d[91];
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	d[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		d[i] = d[i - 1] + d[i - 2];
+	}
+
+	cout << d[n];
+	return 0;
+}
