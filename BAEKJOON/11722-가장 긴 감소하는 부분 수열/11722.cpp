39c57bb27b779dac5806bfb7a9c05fe5ed350425
SeminCha <cing9393@gmail.com>
2018-10-03 15:56:13 +0900

    [11053, 11722 - 증가&감소 부분 수열] dp로 해결

---
+++ BAEKJOON/11722-가장 긴 감소하는 부분 수열/11722.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+	vector<int> v;
+	vector<int> d(1001);
+	int n,a;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		v.push_back(a);
+	}
+
+	for (int i = 0; i < v.size(); i++) {
+		d[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (v[i] < v[j] && d[i] < d[j]+1) {
+				d[i] = d[j] + 1;
+			}
+		}
+	}
+
+	cout << *max_element(d.begin(), d.end()) << "\n";
+
+	return 0;
+}
